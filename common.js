ea31bca8a88bc16c2c56ad29b39292e8ed35d9b1
Tamce <tamce@outlook.com>
2019-12-08 17:26:14 +0800

    Fix typos, add some pages

--- common.js
+++ common.js
@@ -23,7 +23,12 @@ window.dict = {
   "Contacts": "联系我们",
   "- more information -": "- 详情 -",
   "tr-home-1": "华南理工大学队即将参与在2020举行的中东国际太阳能十项全能竞赛（Solar Decathlon Middle Ease，SDME）是以全球高校为参赛单位的太阳能建筑科技竞赛。该竞赛由十个单项比赛组成，因此得名“十项全能”竞赛。竞赛目的是借助世界顶尖研发、设计团队的技术与创意，将太阳能、节能与建筑设计以一体化的新方式紧密结合，设计、建造并运行一座高效节能、由吸引力的太阳能房屋，并希望通过比赛加快太阳能国际化的产学研融合与交流，推进相关技术创新、发展和商业化。",
-  "tr-home-2": "华南理工大学队汇集了华工多方面的力量，包括建筑学院、材料学院、新闻与传播学院的本硕博学生。队伍有专业教师团队指导，并有学校与学院众多机构的支持，包括亚热带建筑科学国家重点实验室、数字化建造实验室、华南理工大学建筑设计研究院有限公司、发光材料与器材国家重点实验室。"
+  "tr-home-2": "华南理工大学队汇集了华工多方面的力量，包括建筑学院、材料学院、新闻与传播学院的本硕博学生。队伍有专业教师团队指导，并有学校与学院众多机构的支持，包括亚热带建筑科学国家重点实验室、数字化建造实验室、华南理工大学建筑设计研究院有限公司、发光材料与器材国家重点实验室。",
+  "Solar Decathlon in U.S": "美国太阳能十项全能竞赛",
+  "Solar Decathlon in China": "中国太阳能十项全能竞赛",
+  "sd-1": "",
+  "sd-2": "",
+  "sd-3": ""
 };
 
 
@@ -91,9 +96,17 @@ function load(page) {
   $("#app").load("./components/" + page + ".html", null, function () {
     reloadLocale();
     reloadRoller();
+    // 加载完毕，如果导航栏处于打开状态，收起之
+    var expanded = Number($(".tmc-navbar").attr("data-expanded"));
+    if (expanded == 0) return;
+    $(".tmc-navbar").animate({
+      right: expanded == 0 ? "0px" : "-85vw"
+    });
+    $(".tmc-navbar").attr("data-expanded", 1 - expanded);
   });
 }
 window.onhashchange = function (e) {
+  if (window.location.hash == "") return;
   var hash = window.location.hash.substr(1);
   // 用于给各个单独的组件取消注册一些定时器之类的
   if (typeof window.beforeLoad == "function") {
@@ -105,5 +118,5 @@ window.onhashchange = function (e) {
 // 在页面载入完成后执行各组件的初始化
 $(function () {
   // load 完成后会自动重新载入 roller 和 locale，这里就不用手动重载入了
-  load();
+  load(window.location.hash.substr(1));
 });
