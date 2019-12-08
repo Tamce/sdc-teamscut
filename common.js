bf87ecc18d1d16707a2b2999d02f746ec42c9ff2
Tamce <tamce@outlook.com>
2019-12-09 00:32:31 +0800

    refactor translate system

--- common.js
+++ common.js
@@ -1,36 +1,12 @@
 window.dict = {
-  "Contest Information": "赛事信息",
-  "Solar Decathlon": "太阳能十项全能竞赛",
-  "2020 Solar Decathlon Middle East": "2020 中东太阳能十项全能竞赛",
-  "About us": "关于我们",
-  "South China University of Technology": "华南理工大学",
-  "Team x SCUT": "华南理工大学队",
-  "Team Members": "队伍成员",
-  "Project": "项目介绍",
-  "Work Schedule": "工作日程",
-  "Concept": "项目概念",
-  "Activities": "活动",
-  "Media Engagement": "Media Engagement",
-  "Offical Activities": "官方活动",
-  "Offline Activities": "线下活动",
-  "Former Works": "往届成果",
-  "SDC 2013": "2013 中国太阳能十项全能竞赛",
-  "SDC 2018": "2018 中国太阳能十项全能竞赛",
-  "Support": "支持我们",
-  "Sponsors": "赞助商",
-  "Media Partners": "合作媒体",
-  "Support TeamSCUT": "支持我们",
-  "Contacts": "联系我们",
   "- more information -": "- 详情 -",
   "tr-home-1": "华南理工大学队即将参与在2020举行的中东国际太阳能十项全能竞赛（Solar Decathlon Middle Ease，SDME）是以全球高校为参赛单位的太阳能建筑科技竞赛。该竞赛由十个单项比赛组成，因此得名“十项全能”竞赛。竞赛目的是借助世界顶尖研发、设计团队的技术与创意，将太阳能、节能与建筑设计以一体化的新方式紧密结合，设计、建造并运行一座高效节能、由吸引力的太阳能房屋，并希望通过比赛加快太阳能国际化的产学研融合与交流，推进相关技术创新、发展和商业化。",
   "tr-home-2": "华南理工大学队汇集了华工多方面的力量，包括建筑学院、材料学院、新闻与传播学院的本硕博学生。队伍有专业教师团队指导，并有学校与学院众多机构的支持，包括亚热带建筑科学国家重点实验室、数字化建造实验室、华南理工大学建筑设计研究院有限公司、发光材料与器材国家重点实验室。",
   "Solar Decathlon in U.S": "美国太阳能十项全能竞赛",
   "Solar Decathlon in China": "中国太阳能十项全能竞赛",
-  "sd-1": "",
-  "sd-2": "",
-  "sd-3": "",
-  "this is a title": "这是标题",
-  "tr-example": "这是描述文本的翻译啊啊啊啊"
+  "sd-1": "国际太阳能十项全能竞赛（Solar Decathlon，SD）是由美国能源部发起并主办的，以全球高校为参赛单位的太阳能建筑科技竞赛。",
+  "sd-2": "该竞赛由十个单项比赛组成，因此得名“十项全能”竞赛。目的是借助世界顶尖研发、设计团队的技术与创意，将太阳能、节能与建筑设计以一体化的新方式紧密结合，设计、建造并运行一座高效节能、由吸引力的太阳能房屋，并希望通过比赛加快太阳能国际化的产学研融合与交流，推进相关技术创新、发展和商业化。",
+  "sd-3": "自2002年开始，大赛在美国、欧洲、中国、中东等国家地区成功举办，吸引了来自全球100多所大学参加比赛，展示了世界最新能源技 术成果以及能源节约技术。"
 };
 
 
@@ -43,13 +19,19 @@ window.tr = function (el) {
     key = en;
   }
   if (window.zh) {
-    $(el).text(window.dict[key]);
+    if (!!$(el).attr("data-tr-zh")) {
+      $(el).text($(el).attr("data-tr-zh"));
+    } else {
+      $(el).text(window.dict[key]);
+    }
     return;
   }
   $(el).text(en);
 };
-window.toggleLocale = function () {
-  window.zh = !window.zh;
+window.toggleLocale = function (nochange) {
+  if (!nochange) {
+    window.zh = !window.zh;
+  }
   $("[data-tr]").map(function (id, el) {
     tr(el);
   });
@@ -67,14 +49,16 @@ window.reloadLocale = function () {
     } else {
       $(el).text($(el).attr("data-tr"));
     }
+
     $(el).attr("data-tr-inited", "true");
   });
-  $("[data-tr-key]").map(function (i, el) {
+  $("[data-tr-key],[data-tr-zh]").map(function (i, el) {
     if (!!$(el).attr("data-tr-inited"))
       return;
     $(el).attr("data-tr", $(el).text());
     $(el).attr("data-tr-inited", "true");
   });
+  toggleLocale(true);
 };
 
 // 初始化 roller
